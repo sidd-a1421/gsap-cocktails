0937979f2301f1b283306a510f1538cf1515fd96
Achinta <achintasiddhanta0304@gmail.com>
2025-08-11 21:49:24 +0530

    fix navbar padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   },[])  
 
   return (
-    <nav>
+    <nav className='px-5'>
         <div>
             <a href="#home" className='flex items-center gap-2'>
                 <img src="/images/logo.png" alt="logo" />
