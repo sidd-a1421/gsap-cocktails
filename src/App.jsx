07670732b155f936a7b31d1a34d4beda1912dfe0
Achinta <achintasiddhanta0304@gmail.com>
2025-08-13 00:32:22 +0530

    implement footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { ScrollTrigger, SplitText } from "gsap/all"
 import gsap from "gsap"
-import { About, Art, Cocktails, Hero, Menu, Navbar } from "./components";
+import { About, Art, Cocktails, Contact, Hero, Menu, Navbar } from "./components";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +13,7 @@ const App = () => {
       <About />
       <Art />
       <Menu />
+      <Contact />
     </main>
   )
 }
